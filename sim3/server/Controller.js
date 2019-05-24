b06e53f2cb605f3fdface5580d92eb3a72e46c01
Jeoffrey <jeoff.howe@gmail.com>
2019-05-24 16:05:56 -0600

    setting up session

--- sim3/server/Controller.js
+++ sim3/server/Controller.js
@@ -10,8 +10,27 @@ module.exports = {
             username,
             hash
         })
+    
+        session.user = user_login[0]
+         
+        res.status(200)
+
     },
-    login:()=>{},
+    login: async (req,res)=>{
+        const db = req.app.get('db')
+        const {username} = req.body
+        const {session} = req
+        try {
+            let user = await db.login({username})
+            session.user = user[0]
+            const authenticated = bcrypt.compareSync(req.body.password,user[0].password)
+
+            if(authenticated) {
+                res.status(200).send({authenticated,username,profile,userId})
+            } else {throw new Error (401,'Error on login')}
+        }catch(err) {res.sendStatus(401)}
+
+    },
     getPost: ()=>{},
     createPost: ()=>{},
     getUser:()=>{}
